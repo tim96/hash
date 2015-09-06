79e697081b50fb4f8e6d21591c538f79bdd3c084
Alexander <tim96@yandex.ru>
2015-09-06 21:36:19 +0400

    Add example how to calculate execution time.

--- web/index.php
+++ web/index.php
@@ -1,5 +1,8 @@
 <?php
 
+// Calculate execution time
+// $time_start = microtime(true);
+
 ini_set('display_errors', 0);
 
 require_once __DIR__.'/../vendor/autoload.php';
@@ -9,3 +12,5 @@ require __DIR__.'/../app/config/config_prod.php';
 
 $app->run();
 
+// $execution_time = (microtime(true) - $time_start);
+// echo '<b>Total Execution Time:</b> '.$execution_time.' Seconds';
