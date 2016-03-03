420edaa281520a84d1b4d8fa941621bafe28e793
Alexander <tim96@yandex.ru>
2016-03-03 09:58:43 +0400

    Show ip address in dev mode.

--- web/index_dev.php
+++ web/index_dev.php
@@ -12,10 +12,10 @@ use Symfony\Component\Debug\Debug;
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '107.15.88.251'))
+    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
 ) {
     header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information. Ip: ' . $_SERVER['REMOTE_ADDR']);
 }
 
 require_once __DIR__.'/../vendor/autoload.php';
